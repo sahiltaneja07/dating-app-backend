10bb697c6d30a847a69d8635e6a2c53ff0855ac6
Pawandeep Kaur <33888316+payalkaur@users.noreply.github.com>
2024-12-24 20:27:44 +0100

    Created schema

---
+++ src/user/schemas/likes-sent.schema.ts
@@ -0,0 +1,15 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import * as mongoose from "mongoose";
+
+@Schema({
+    timestamps: true
+})
+export class LikesSent {
+    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
+    userId: string;
+
+    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
+    like_sent_to: string;
+}
+
+export const LikesSentSchema = SchemaFactory.createForClass(LikesSent);
